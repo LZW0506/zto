0ace71205d8f05953fe178718d056fa0a34736bf
李智伟 <378684147@qq.com>
2023-07-24 16:16:03 +0800

    优化config

--- src/config/zto.php
+++ src/config/zto.php
@@ -48,52 +48,36 @@ return [
         *飞书创建多维表格
         */
         'table_info' => [
-            "name" => "bugList", // 数据表名
-            "default_view_name" => "bugList", // 默认视图名
-
-            /*
-             *  优先级
-             */
-            "pri" => [
-                1 => "1",
-                2 => "2",
-                3 => "3",
-                4 => "4",
-            ],
-
-            /*
-             *  bug状态
-             */
-            "bug_status" => [
-                "active" => "激活",
-                "resolved" => "已解决",
-                "closed" => "已关闭",
-            ],
-
-            /*
-             *  严重程度
-             */
-            "severity" =>  [
-                1 => "1",
-                2 => "2",
-                3 => "3",
-                4 => "4",
-            ],
-
-            /*
-             *  bug类型
-             */
-            "type" => [
-                "codeerror" => "代码错误",
-                "config" => "配置相关",
-                "install" => "安装部署",
-                "security" => "安全相关",
-                "performance" => "性能问题",
-                "standard" => "标准规范",
-                "automation" => "测试脚本",
-                "designdefect" => "设计缺陷",
-                "others" => "其他",
-            ],
+            'name' => '禅道bug', // 数据表名
+            'default_view_name' => 'bugList', // 默认视图名
+            'pri' => [
+                1 => '1',
+                2 => '2',
+                3 => '3',
+                4 => '4',
+            ],// 优先级
+            'bug_status' => [
+                'active' => '激活',
+                'resolved' => '已解决',
+                'closed' => '已关闭',
+            ], // bug状态字段名
+            'severity' => [
+                1 => '1',
+                2 => '2',
+                3 => '3',
+                4 => '4',
+            ], // 严重程度
+            'type' => [
+                'codeerror' => '代码错误',
+                'config' => '配置相关',
+                'install' => '安装部署',
+                'security' => '安全相关',
+                'performance' => '性能问题',
+                'standard' => '标准规范',
+                'automation' => '测试脚本',
+                'designdefect' => '设计缺陷',
+                'others' => '其他',
+            ], // bug类型
         ],
     ],
 ];
